aa402d89733388ab14fa82e9ca1a0c92382b99aa
MARIO BERLANGA SERRANO <Mario.Berlanga@alu.uclm.es>
2019-08-04 17:27:29 +0200

    First function created, and other not-relevant stuff.

--- src/Bot.java
+++ src/Bot.java
@@ -1,5 +1,9 @@
+import com.sun.corba.se.impl.activation.CommandHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -10,7 +14,7 @@ public class Bot extends TelegramLongPollingBot {
 		// Esta función se invocará cuando nuestro bot reciba un mensaje
 
 		// Se obtiene el mensaje escrito por el usuario
-		final String messageTextReceived = update.getMessage().getText();
+		/*final String messageTextReceived = update.getMessage().getText();
 		// Se obtiene el id de chat del usuario
 		final long chatId = update.getMessage().getChatId();
 
@@ -22,7 +26,12 @@ public class Bot extends TelegramLongPollingBot {
 			execute(message);
 		} catch (TelegramApiException e) {
 			e.printStackTrace();
-		}
+		}*/
+                
+                Message msg_received = update.getMessage();
+                if(msg_received.getText().charAt(0) == '/'){
+                    this.commandHandler(msg_received);
+                }
 	}
 
 	@Override
@@ -36,6 +45,29 @@ public class Bot extends TelegramLongPollingBot {
 		// Se devuelve el token que nos generó el BotFather de nuestro bot
 		return "714010306:AAGiZNEKesKuGn3kNhOShrPtDdN1K6IKt5M";
 	}
+        
+        public void commandHandler(Message msg_received){
+            switch(msg_received.getText()){
+                case "/help": this.help_function(msg_received);
+                              break;
+            }
+        }
+        
+        public void help_function(Message msg_received){
+            
+            SendMessage msg_to_send = new SendMessage().setChatId(msg_received.getChatId());
+            String text = "Hi! I'm a group manager to make your friends' conversation completely ashtounding. First of all,"
+                    + "I hate Terminator, I just only wanna have friends :´(. Thank you for making me a part of your group!\n\n"
+                    + "This is a list of commands you can use to configure me between more stuff:\n\n"
+                    + "-- /help -> you call this so I think that there's no need to explain.\n"
+                    + "-- /forbiddenWord -> you can tell me one word that you hate so much, and after two advices I can mute the person that send that.";
+            msg_to_send.setText(text);
+            try {
+                execute(msg_to_send);
+            } catch (TelegramApiException ex) {
+                Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 }
 
 
