ed8f2236dd17138b626dd4a56e2aaf8669f93d68
MARIO BERLANGA SERRANO <mariobasket88@gmail.com>
2019-09-08 20:13:39 +0200

    Forbidden word function finished without errors, not tested

--- src/Bot.java
+++ src/Bot.java
@@ -5,15 +5,25 @@ import java.util.logging.Logger;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
+import java.util.ArrayList;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import com.pengrad.telegrambot.response.BaseResponse;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.KickChatMember;
 
 public class Bot extends TelegramLongPollingBot {
 
+    String forbidden_word = null;
+    boolean flag1 = false;
+    ArrayList participants = new ArrayList();
+    ArrayList<Integer> advises = new ArrayList();
+    int contMsg = 0;
+                        
+    
 	@Override
 	public void onUpdateReceived(final Update update) {
 		// Esta función se invocará cuando nuestro bot reciba un mensaje
@@ -32,9 +42,91 @@ public class Bot extends TelegramLongPollingBot {
 		 */
 
 		Message msg_received = update.getMessage();
-		if (msg_received.getText().charAt(0) == '/') {
-			this.commandHandler(msg_received);
+                String text_received = msg_received.getText();
+                Long chatID = msg_received.getChatId();
+		if (text_received.charAt(0) == '/') {
+                    try {
+                        this.commandHandler(msg_received);
+                    } catch (TelegramApiException ex) {
+                        Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
+                    }
 		}
+                if(flag1){
+                    switch(text_received.charAt(0)){
+                        case '1':
+                            forbidden_word = text_received.substring(3);
+                            break;
+                        case '2':
+                            forbidden_word = null;
+                            break;
+                        case '3':
+                            SendMessage checkWord = new SendMessage();
+                            checkWord.setChatId(chatID);
+                            checkWord.setText(forbidden_word);
+                            break;
+                        default:
+                            SendMessage errorMsg = new SendMessage();
+                            errorMsg.setChatId(chatID);
+                            errorMsg.setText("Something went wrong, try to execute again the command");
+                    {
+                        try {
+                            execute(errorMsg);
+                        } catch (TelegramApiException ex) {
+                            Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                            break;
+                    }
+                    flag1 = false;
+                }
+                else{
+                    if(text_received.contains(forbidden_word)){
+                        if (participants.contains(msg_received.getFrom())){
+                            int ind = participants.indexOf(msg_received.getFrom());
+                            if (advises.get(ind) == 1){
+                                SendMessage kickMsg = new SendMessage();
+                                kickMsg.setChatId(chatID);
+                                kickMsg.setText("Ah shit, here we go again.");
+                                try {
+                                    execute(kickMsg);
+                                } catch (TelegramApiException ex) {
+                                    Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                                advises.set(ind, 0);
+                                KickChatMember kick = new KickChatMember(chatID, msg_received.getFrom().getId());
+                                try {
+                                    execute(kick);
+                                } catch (TelegramApiException ex) {
+                                    Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                            }else {
+                                SendMessage advMsg = new SendMessage();
+                                advMsg.setChatId(chatID);
+                                advMsg.setText("First advise, don´t write again " + forbidden_word);
+                                try {
+                                    execute(advMsg);
+                                } catch (TelegramApiException ex) {
+                                    Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                                advises.set(ind, 1);
+                            }
+                        }else{
+                            participants.add(msg_received.getFrom());
+                            advises.add(1);
+                            SendMessage advMsg = new SendMessage();
+                                advMsg.setChatId(chatID);
+                                advMsg.setText("First advise, don´t write again " + forbidden_word);
+                                try {
+                                    execute(advMsg);
+                                } catch (TelegramApiException ex) {
+                                    Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                        }
+                        
+                    }
+                }
+                
+                
 	}
 
 	@Override
@@ -49,11 +141,18 @@ public class Bot extends TelegramLongPollingBot {
 		return "myToken";
 	}
 
-	public void commandHandler(Message msg_received) {
+	public void commandHandler(Message msg_received) throws TelegramApiException {
+            Long chatID = msg_received.getChatId();
 		switch (msg_received.getText()) {
 		case "/help":
-			this.help_function(msg_received.getChatId());
+			this.help_function(chatID);
 			break;
+                default: 
+                    SendMessage errorMsg = new SendMessage();
+                    errorMsg.setChatId(chatID);
+                    errorMsg.setText("That's not a command! Check your writing!");
+                    execute(errorMsg);
+                        
 		}
 	}
 
@@ -74,28 +173,16 @@ public class Bot extends TelegramLongPollingBot {
 		}
 	}
 	
-	public String forbiddenWord(Long id_chat){
-		String strResult = null;
+	public String forbiddenWord(Long id_chat) throws InterruptedException{
+		flag1 = true;
+                String strResult = null;
 		SendMessage msgFirst = new SendMessage().setChatId(id_chat);
-		msgFirst.setText("Please, tell me the word that you hate");
+		msgFirst.setText("What do you want to do?\n- Add forbidden word, write the number 1 and your word after a space (1 Silly)\n- Delete forbidden word, write the number 2 and your word after a space\n- Check your forbidden word, write the number 3\n");
 		//Aqui va lo del sticker
-
-		new UpdatesListener(){
-		
-			@Override
-			public int process(List<com.pengrad.telegrambot.model.Update> update) {
-				// TODO Auto-generated method stub
-				String msgReceived = ((Throwable) update).getMessage();
-				System.out.println(msgReceived);
-				return 0;
-			}
-		};
-		
-		
-
-
-
-		return strResult;
+                
+                this.wait();
+                
+                return strResult;
 
 	}
 
