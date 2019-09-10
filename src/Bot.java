ff8a10a0aed961b08f5686368e27f2f781236497
MARIO BERLANGA SERRANO <Mario.Berlanga@alu.uclm.es>
2019-09-10 14:26:20 +0200

    Errors in running the forbidden word function.

--- src/Bot.java
+++ src/Bot.java
@@ -21,8 +21,7 @@ public class Bot extends TelegramLongPollingBot {
     boolean flag1 = false;
     ArrayList participants = new ArrayList();
     ArrayList<Integer> advises = new ArrayList();
-    int contMsg = 0;
-                        
+    int contMsg = 0;                  
     
 	@Override
 	public void onUpdateReceived(final Update update) {
@@ -44,20 +43,29 @@ public class Bot extends TelegramLongPollingBot {
 		Message msg_received = update.getMessage();
                 String text_received = msg_received.getText();
                 Long chatID = msg_received.getChatId();
+                
 		if (text_received.charAt(0) == '/') {
                     try {
                         this.commandHandler(msg_received);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
                     } catch (TelegramApiException ex) {
                         Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
                     }
 		}
-                if(flag1){
+                if(flag1 && msg_received.getFrom().getBot()){
                     switch(text_received.charAt(0)){
                         case '1':
                             forbidden_word = text_received.substring(3);
+                            SendMessage correct1 = new SendMessage();
+                            correct1.setChatId(chatID);
+                            correct1.setText("Done!");
                             break;
                         case '2':
                             forbidden_word = null;
+                            SendMessage correct2 = new SendMessage();
+                            correct2.setChatId(chatID);
+                            correct2.setText("Done!");
                             break;
                         case '3':
                             SendMessage checkWord = new SendMessage();
@@ -80,7 +88,7 @@ public class Bot extends TelegramLongPollingBot {
                     flag1 = false;
                 }
                 else{
-                    if(text_received.contains(forbidden_word)){
+                    if(forbidden_word != null && text_received.contains(forbidden_word)){
                         if (participants.contains(msg_received.getFrom())){
                             int ind = participants.indexOf(msg_received.getFrom());
                             if (advises.get(ind) == 1){
@@ -138,15 +146,18 @@ public class Bot extends TelegramLongPollingBot {
 	@Override
 	public String getBotToken() {
 		// Se devuelve el token que nos generó el BotFather de nuestro bot
-		return "myToken";
+		return "714010306:AAGiZNEKesKuGn3kNhOShrPtDdN1K6IKt5M";
 	}
 
-	public void commandHandler(Message msg_received) throws TelegramApiException {
+	public void commandHandler(Message msg_received) throws TelegramApiException, InterruptedException {
             Long chatID = msg_received.getChatId();
 		switch (msg_received.getText()) {
 		case "/help":
 			this.help_function(chatID);
 			break;
+                case "/forbiddenword":
+                        this.forbiddenWord(chatID);
+                        break;
                 default: 
                     SendMessage errorMsg = new SendMessage();
                     errorMsg.setChatId(chatID);
@@ -159,12 +170,12 @@ public class Bot extends TelegramLongPollingBot {
 	public void help_function(Long id_chat) {
 
 		SendMessage msg_to_send = new SendMessage().setChatId(id_chat);
-		String text = "Hi! I'm a group manager to make your friends' conversation completely ashtounding. First of all,"
+		String text = "Hi! I'm a group manager to make your friends' conversation completely ashtounding. First of all, "
 				+ "I hate Terminator, I just only wanna have friends :´(. Thank you for making me a part of your group!\n\n"
 				+ "This is a list of commands you can use to configure me between more stuff:\n\n"
 				+ "-- /help -> you call this so I think that there's no need to explain.\n"
-				+ "-- /forbiddenWord -> you can tell me one word that you hate so much, and after two advices I can mute the person that send that.\n"
-				+ "-- /theGame -> write the name and the times that that person lose in a table.";
+				+ "-- /forbiddenword -> you can tell me one word that you hate so much, and after two advices I can mute the person that send that.\n"
+				+ "-- /thegame -> write the name and the times that that person lose in a table.";
 		msg_to_send.setText(text);
 		try {
 			execute(msg_to_send);
@@ -173,16 +184,13 @@ public class Bot extends TelegramLongPollingBot {
 		}
 	}
 	
-	public String forbiddenWord(Long id_chat) throws InterruptedException{
-		flag1 = true;
-                String strResult = null;
+	public void forbiddenWord(Long id_chat) throws InterruptedException, TelegramApiException{
+                flag1 = true;
 		SendMessage msgFirst = new SendMessage().setChatId(id_chat);
 		msgFirst.setText("What do you want to do?\n- Add forbidden word, write the number 1 and your word after a space (1 Silly)\n- Delete forbidden word, write the number 2 and your word after a space\n- Check your forbidden word, write the number 3\n");
+                execute(msgFirst);
 		//Aqui va lo del sticker
                 
-                this.wait();
-                
-                return strResult;
 
 	}
 
